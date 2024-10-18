30e5ab29abf5f0bcadeae610b6252ff0257b5753
ajarmeh-oroub <ajarmehoroub12@gmail.com>
2024-10-18 16:17:19 +0300

    update

---
+++ conn.php
@@ -0,0 +1,19 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbnam="ecommerce";
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
+
+
