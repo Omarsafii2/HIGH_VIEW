acaa1f15ae22da39ec2451ee43ca170237d6664b
ajarmeh-oroub <ajarmehoroub12@gmail.com>
2024-10-23 21:43:49 +0300

    tttttttt

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,5 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
 
     <!-- Footer -->
 <footer class="text-center text-lg-start text-white" style="background-color: #1c2331;">
